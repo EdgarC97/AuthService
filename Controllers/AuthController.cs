c811f0ff4a728e5bbe50462123b7654a4c5e816c
EdgarC97 <ecardona742@gmail.com>
2025-03-01 23:34:43 -0500

    feat: separte requests logic

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using AuthService.Models;
 using AuthService.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using AuthService.Models.DTOs;
 
 namespace AuthService.Controllers
 {
@@ -51,19 +52,4 @@ namespace AuthService.Controllers
         }
     }
 
-    // Request models for authentication
-    public class RegisterRequest
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-
-    public class LoginRequest
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
 }
---
+++ Models/DTOs/AuthRequests.cs
@@ -0,0 +1,18 @@
+﻿namespace AuthService.Models.DTOs
+{
+    public class RegisterRequest
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
